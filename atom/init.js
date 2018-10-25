0d1a16636aeb2e3a225eb4392370351ddadc5b2a
t9md <taqumd@gmail.com>
2018-10-25 18:07:43 +0900

    add garbages

--- atom/init.js
+++ atom/init.js
@@ -38,6 +38,31 @@ function consumeVimModePlusService(fn) {
   }
 }
 
+
+// consumeService('status-bar', "provideStatusBar", service => {
+//   const item = document.createElement('div')
+//   item.className = 'inline-block'
+//
+//   const update = editor => {
+//     let text = ""
+//     if (editor) {
+//       // Show "TextMate" or "TreeSitter" on status-bar.
+//       text = editor.languageMode.constructor.name.replace(/LanguageMode$/, "")
+//     }
+//     item.textContent = text
+//   }
+//
+//   atom.workspace.observeTextEditors(editor => {
+//     editor.onDidChangeGrammar(() => {
+//       if (atom.workspace.getActiveTextEditor() === editor) {
+//         update(editor)
+//       }
+//     })
+//   })
+//   atom.workspace.onDidChangeActiveTextEditor(update)
+//   service.addLeftTile({item})
+// })
+
 consumeVimModePlusService(service => {
   return
   // To evaluate constom-operation feat
@@ -131,6 +156,65 @@ async function hotReloadPackages() {
   }
 }
 
+function toggleSuppressAutoCompletePlus() {
+  const className = "suppress-autocomplete-plus"
+  const editor = atom.workspace.getActiveTextEditor()
+  const suppressed = editor.element.classList.contains(className)
+  editor.element.classList.toggle(className, !suppressed)
+}
+
+class SyntaxNodeExproler {
+  static create() {
+    global = new this()
+  }
+
+  constructor() {
+    this.marker = null
+    this.node = null
+  }
+
+  markParent() {
+    const editor = atom.workspace.getActiveTextEditor()
+
+    let node
+
+    if (this.node) {
+      if (this.node.parent) {
+        node = this.node.parent
+      } else {
+        console.log("no more parent!");
+        return
+      }
+    } else {
+      node = editor.languageMode.getSyntaxNodeAtPosition(editor.getCursorBufferPosition())
+    }
+
+    if (this.marker) {
+      this.marker.destroy()
+    }
+    console.log({'nodeType': node.type, 'named': node.isNamed});
+    this.node = node
+    this.marker = editor.markBufferRange(node.range)
+    editor.decorateMarker(this.marker, {
+      type: 'highlight',
+      class: 'vim-mode-plus-highlight-search',
+    })
+
+  }
+
+  clear() {
+    if (this.marker) {
+      this.marker.destroy()
+      this.marker = null
+    }
+    if (this.node) {
+      this.node = null
+    }
+    console.log('clear')
+  }
+}
+global._sn = new SyntaxNodeExproler()
+
 atom.commands.add("atom-workspace", {
   "user:hello"() {
     console.log("hello!")
@@ -138,12 +222,31 @@ atom.commands.add("atom-workspace", {
   "user:clear-console"() {
     console.clear()
   },
+  "user:sn-mark-parent"() {
+    global._sn.markParent()
+  },
+  "user:sn-clear"() {
+    global._sn.clear()
+  },
   "user:toggle-show-invisible"() {
     const param = "editor.showInvisibles"
     atom.config.set(param, !atom.config.get(param))
   },
+  "user:toggle-use-tree-sitter-parsers"() {
+    const param = "core.useTreeSitterParsers"
+    atom.config.set(param, !atom.config.get(param))
+    console.log(param, atom.config.get(param))
+  },
+  "user:report-language-mode"() {
+    const editor = atom.workspace.getActiveTextEditor()
+    const name = editor.languageMode.constructor.name
+    console.log(name);
+  },
   "user:package-hot-reload"() {
     hotReloadPackages()
+  },
+  "user:toggle-suppress-auto-complete-plus"() {
+    toggleSuppressAutoCompletePlus()
   },
 })
 
